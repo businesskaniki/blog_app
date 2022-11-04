5f72d92b2ba97b9cf05eaa952cb51d77b4acbe44
businesskanki <businesskanki@gmail.com>
2022-11-04 16:33:51 +0300

    adding views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   # resources :posts
   # resources :comments
   # resources :users
-  # root 'users#index'
+  # # root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show]
-
+  end
+  root 'users#index'
 end
